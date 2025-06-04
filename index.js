971437339431c10cae4cd97e1729ca75b3b3cda2
umang <umangdhandhukiya97@gmail.com>
2025-06-04 23:42:52 +0530

    api call for playing cards

--- index.js
+++ index.js
@@ -10,4 +10,39 @@ function onHandle(event) {
   }
 }
 
- document.getElementById("user").innerHTML = localStorage.getItem("user");
+document.getElementById("user").innerHTML = localStorage.getItem("user");
+
+async function shuffle() {
+  const data = await fetch(
+    "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
+  );
+  const json = await data.json();
+  console.log(json);
+}
+shuffle();
+
+async function drawCard() {
+  const data = await fetch(
+    "https://www.deckofcardsapi.com/api/deck/3teevlx9e2co/draw/?count=3"
+  );
+  const json = await data.json();
+  console.log(json);
+
+  json?.cards.map((card, index) => {
+    if (index == 0) {
+      const dealer = document.getElementById("left");
+      const img = document.createElement("img");
+      img.src = card?.image;
+      dealer.appendChild(img);
+    } else {
+      const User = document.getElementById("userCard");
+      const img = document.createElement("img");
+      img.src = card?.image;
+      User.appendChild(img);
+    }
+  });
+
+  // const image = document.getElementById("dealerCard");
+  // image.src = json?.cards[0]?.image
+}
+drawCard();
